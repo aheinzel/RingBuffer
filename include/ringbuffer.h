2405060cfcf383235eb1396a73cb920e2ad97293
Andreas Heinzel <1409997+aheinzel@users.noreply.github.com>
2022-07-27 05:04:15 +0200

    add dynamic ring buffer

--- include/ringbuffer.h
+++ include/ringbuffer.h
@@ -1,15 +1,14 @@
-#pragma once
-
 #include<assert.h>
+#include<cstddef>
 
-template<size_t requestedCapacity, typename T>
+template<typename T>
 class RingBuffer{
 private:
-    T _buffer[requestedCapacity];
+    T * _buffer = 0;
+    const size_t _capacity;
     bool _full = false;
     size_t _head = 0;
     size_t _tail = 0;
-    const size_t _capacity = requestedCapacity;
     
     size_t increment(size_t & i){
         ++i;
@@ -20,7 +19,26 @@ private:
         return i;
     }
 
+    size_t decrement(size_t & i){
+        i = (i == 0 ? capacity() : i) - 1;
+        return i;
+    }
+
+protected:
+    RingBuffer(T * buffer, const RingBuffer & other)
+        : _buffer(buffer), _capacity(other._capacity), _full(other._full), _head(other._head), _tail(other._tail){
+        assert(_capacity == other._capacity);
+        for(size_t i = 0; i < _capacity; ++i){
+            _buffer[i] = other._buffer[i];
+        }
+    }
+
 public:
+    RingBuffer(T * buffer, size_t capacity): _buffer(buffer), _capacity(capacity){
+    }
+
+    virtual ~RingBuffer() = 0;
+
     size_t capacity() const{
         return _capacity;
     }
@@ -59,9 +77,76 @@ public:
         _full = _head == _tail;
     }
 
+    const T & peek(){
+        assert(!empty());
+        return _buffer[_tail];
+    }
+
+    const T & getFront(){
+        assert(!empty());
+        decrement(_head);
+        _full = false;
+        return _buffer[_head];
+    }
+
+    const T & peekFront(){
+        assert(!empty());
+        size_t tmp = _head;
+        decrement(tmp);
+        return _buffer[tmp];
+    }
+
     void reset(){
         _full = false;
         _head = 0;
         _tail = 0;
+    }
+};
+
+template<typename T>
+RingBuffer<T>::~RingBuffer(){}
+
+
+template<size_t requestedCapacity, typename T>
+class StaticRingBuffer : public RingBuffer<T>{
+public:
+    T _buffer[requestedCapacity];
+ 
+ public:
+    StaticRingBuffer(): RingBuffer<T>(_buffer, requestedCapacity){
+    }
+
+    StaticRingBuffer(const StaticRingBuffer & other): RingBuffer<T>(_buffer, other){
+    }
+
+    virtual ~StaticRingBuffer(){
+    }
+};
+
+
+template<typename T>
+class DynamicRingBuffer : public RingBuffer<T>{
+private:
+    T * _buffer;
+    bool _deleteBuffer;
+
+    DynamicRingBuffer(T * buffer, const DynamicRingBuffer & other): _buffer(buffer), _deleteBuffer(true), RingBuffer<T>(buffer, other){
+    }
+
+ public:
+    DynamicRingBuffer(size_t capacity): DynamicRingBuffer<T>(new T [capacity], true, capacity){
+    }
+
+    DynamicRingBuffer(T * buffer, bool takeBufferOwnership, size_t capacity): _buffer(buffer), _deleteBuffer(takeBufferOwnership), RingBuffer<T>(buffer, capacity){
+    }
+
+    DynamicRingBuffer(const DynamicRingBuffer & other): DynamicRingBuffer<T>(new T [other.capacity()], other){
+    }
+
+    virtual ~DynamicRingBuffer(){
+        if(_deleteBuffer){
+            delete _buffer;
+            _buffer = nullptr;
+        }
     }
 };
--- test/ringbuffer_test.cpp
+++ test/ringbuffer_test.cpp
@@ -1,123 +1,284 @@
 #include <gtest/gtest.h>
 #include "ringbuffer.h"
 
+//TODO replace "factories" with cleaner version using traits
+struct StaticRingBufferFactory{
+  static RingBuffer<int> * create2(){
+    return new StaticRingBuffer<2, int>();
+  }
 
-TEST(RingBuffer, capacity) {
-  RingBuffer<2, int> rb2;
-  RingBuffer<5, int> rb5;
-  EXPECT_EQ(2, rb2.capacity());
-  EXPECT_EQ(5, rb5.capacity());
+  static RingBuffer<int> * create3(){
+    return new StaticRingBuffer<3, int>();
+  }
+
+  static RingBuffer<int> * create5(){
+    return new StaticRingBuffer<5, int>();
+  }
+};
+
+
+struct DynamicRingBufferFactory{
+  static RingBuffer<int> * create2(){
+    return create(2);
+  }
+
+  static RingBuffer<int> * create3(){
+    return create(3);
+  }
+
+  static RingBuffer<int> * create5(){
+    return create(5);
+  }
+
+  static RingBuffer<int> * create(const size_t capacity){
+    return new DynamicRingBuffer<int>(capacity);
+  }
+};
+
+
+template <typename T>
+class RingBufferTest : public testing::Test {
+ public:
+  RingBuffer<int> * rb2 = T::create2();
+  RingBuffer<int> * rb3 = T::create3();
+  RingBuffer<int> * rb5 = T::create5();
+
+  virtual ~ RingBufferTest(){
+    delete rb2;
+    rb2 = nullptr;
+    delete rb3;
+    rb3 = nullptr;
+    delete rb5;
+    rb5 = nullptr;
+  }
+};
+
+
+using RingBufferTypes = ::testing::Types<StaticRingBufferFactory, DynamicRingBufferFactory>;
+TYPED_TEST_SUITE(RingBufferTest, RingBufferTypes);
+
+
+TYPED_TEST(RingBufferTest, capacity) {
+  EXPECT_EQ(2, this -> rb2 -> capacity());
+  EXPECT_EQ(5, this -> rb5 -> capacity());
 }
 
-TEST(RingBuffer, empty) {
-  RingBuffer<2, int> rb;
-  EXPECT_TRUE(rb.empty());
-  rb.put(1);
-  EXPECT_FALSE(rb.empty());
+
+TYPED_TEST(RingBufferTest, empty) {
+  EXPECT_TRUE(this -> rb2 -> empty());
+  this -> rb2 -> put(1);
+  EXPECT_FALSE(this -> rb2 -> empty());
 }
 
-TEST(RingBuffer, full) {
-  RingBuffer<2, int> rb;
-  EXPECT_FALSE(rb.full());
-  rb.put(1);
-  EXPECT_FALSE(rb.full());
-  rb.put(2);
-  EXPECT_TRUE(rb.full());
+
+TYPED_TEST(RingBufferTest, full) {
+  EXPECT_FALSE(this -> rb2 -> full());
+  this -> rb2 -> put(1);
+  EXPECT_FALSE(this -> rb2 -> full());
+  this -> rb2 -> put(2);
+  EXPECT_TRUE(this -> rb2 -> full());
 }
 
-TEST(RingBuffer, size) {
-  RingBuffer<3, int> rb;
-  EXPECT_EQ(0, rb.size());
-  rb.put(9);
-  EXPECT_EQ(1, rb.size());
-  rb.put(8);
-  EXPECT_EQ(2, rb.size());
-  rb.put(7);
-  EXPECT_EQ(3, rb.size());
-  rb.get();
-  EXPECT_EQ(2, rb.size());
+
+TYPED_TEST(RingBufferTest, size) {
+  EXPECT_EQ(0, this -> rb3 -> size());
+  this -> rb3 -> put(9);
+  EXPECT_EQ(1, this -> rb3 -> size());
+  this -> rb3 -> put(8);
+  EXPECT_EQ(2, this -> rb3 -> size());
+  this -> rb3 -> put(7);
+  EXPECT_EQ(3, this -> rb3 -> size());
+  this -> rb3 -> get();
+  EXPECT_EQ(2, this -> rb3 -> size());
 }
 
-TEST(RingBuffer, put) {
-  RingBuffer<3, int> rb;
-  rb.put(1);
-  rb.put(2);
-  rb.put(3);
 
-  EXPECT_EQ(1, rb.get());
-  EXPECT_EQ(2, rb.get());
-  EXPECT_EQ(3, rb.get());
+TYPED_TEST(RingBufferTest, put) {
+  this -> rb3 -> put(1);
+  this -> rb3 -> put(2);
+  this -> rb3 -> put(3);
+
+  EXPECT_EQ(1, this -> rb3 -> get());
+  EXPECT_EQ(2, this -> rb3 -> get());
+  EXPECT_EQ(3, this -> rb3 -> get());
 }
 
-TEST(RingBuffer, putWrap) {
-  RingBuffer<3, int> rb;
-  rb.put(1);
-  rb.put(2);
-  rb.put(3);
-  rb.put(4);
-  rb.put(5);
 
-  EXPECT_EQ(3, rb.get());
-  EXPECT_EQ(4, rb.get());
-  EXPECT_EQ(5, rb.get());
+TYPED_TEST(RingBufferTest, putWrap) {
+  this -> rb3 -> put(1);
+  this -> rb3 -> put(2);
+  this -> rb3 -> put(3);
+  this -> rb3 -> put(4);
+  this -> rb3 -> put(5);
+
+  EXPECT_EQ(3, this -> rb3 -> get());
+  EXPECT_EQ(4, this -> rb3 -> get());
+  EXPECT_EQ(5, this -> rb3 -> get());
 }
 
-TEST(RingBuffer, putIntermedGetWrap) {
-  RingBuffer<3, int> rb;
-  EXPECT_TRUE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(0, rb.size());
-  rb.put(1);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(1, rb.size());
+
+TYPED_TEST(RingBufferTest, putIntermedGetWrap) {
+  EXPECT_TRUE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(0, this -> rb3 -> size());
+  this -> rb3 -> put(1);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(1, this -> rb3 -> size());
   
-  rb.put(2);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(2, rb.size());
+  this -> rb3 -> put(2);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(2, this -> rb3 -> size());
   
-  EXPECT_EQ(1, rb.get());
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(1, rb.size());
+  EXPECT_EQ(1, this -> rb3 -> get());
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(1, this -> rb3 -> size());
   
-  rb.put(3);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(2, rb.size());
+  this -> rb3 -> put(3);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(2, this -> rb3 -> size());
 
-  EXPECT_EQ(2, rb.get());
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(1, rb.size());
+  EXPECT_EQ(2, this -> rb3 -> get());
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(1, this -> rb3 -> size());
 
-  EXPECT_EQ(3, rb.get());
-  EXPECT_TRUE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(0, rb.size());
+  EXPECT_EQ(3, this -> rb3 -> get());
+  EXPECT_TRUE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(0, this -> rb3 -> size());
   
-  rb.put(4);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(1, rb.size());
+  this -> rb3 -> put(4);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(1, this -> rb3 -> size());
 
-  rb.put(5);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_FALSE(rb.full());
-  EXPECT_EQ(2, rb.size());
+  this -> rb3 -> put(5);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_FALSE(this -> rb3 -> full());
+  EXPECT_EQ(2, this -> rb3 -> size());
 
-  rb.put(6);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_TRUE(rb.full());
-  EXPECT_EQ(3, rb.size());
+  this -> rb3 -> put(6);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_TRUE(this -> rb3 -> full());
+  EXPECT_EQ(3, this -> rb3 -> size());
 
-  rb.put(7);
-  EXPECT_FALSE(rb.empty());
-  EXPECT_TRUE(rb.full());
-  EXPECT_EQ(3, rb.size());
+  this -> rb3 -> put(7);
+  EXPECT_FALSE(this -> rb3 -> empty());
+  EXPECT_TRUE(this -> rb3 -> full());
+  EXPECT_EQ(3, this -> rb3 -> size());
 
-  EXPECT_EQ(5, rb.get());
-  EXPECT_EQ(6, rb.get());
-  EXPECT_EQ(7, rb.get());
+  EXPECT_EQ(5, this -> rb3 -> get());
+  EXPECT_EQ(6, this -> rb3 -> get());
+  EXPECT_EQ(7, this -> rb3 -> get());
+}
+
+
+TYPED_TEST(RingBufferTest, getFront) {
+  this -> rb3 -> put(9);
+  EXPECT_EQ(9, this -> rb3 -> getFront());
+  this -> rb3 -> put(8);
+  this -> rb3 -> put(7);
+  EXPECT_EQ(7, this -> rb3 -> getFront());
+  EXPECT_EQ(8, this -> rb3 -> get());
+  this -> rb3 -> put(6);
+  this -> rb3 -> put(5);
+  EXPECT_EQ(5, this -> rb3 -> getFront());
+  EXPECT_EQ(6, this -> rb3 -> getFront());
+}
+
+
+TYPED_TEST(RingBufferTest, peek) {
+  this -> rb3 -> put(9);
+  EXPECT_EQ(9, this -> rb3 -> peek());
+  EXPECT_EQ(9, this -> rb3 -> peek());
+  this -> rb3 -> put(8);
+  EXPECT_EQ(9, this -> rb3 -> peek());
+  EXPECT_EQ(8, this -> rb3 -> getFront());
+  EXPECT_EQ(9, this -> rb3 -> peek());
+  this -> rb3 -> put(7);
+  EXPECT_EQ(9, this -> rb3 -> peek());
+  EXPECT_EQ(9, this -> rb3 -> get());
+  EXPECT_EQ(7, this -> rb3 -> peek());
+}
+
+
+TYPED_TEST(RingBufferTest, peekFront) {
+  this -> rb3 -> put(9);
+  this -> rb3 -> put(8);
+  EXPECT_EQ(8, this -> rb3 -> peekFront());
+  EXPECT_EQ(8, this -> rb3 -> peekFront());
+  this -> rb3 -> put(7);
+  EXPECT_EQ(7, this -> rb3 -> peekFront());
+  EXPECT_EQ(9, this -> rb3 -> get());
+  EXPECT_EQ(7, this -> rb3 -> peekFront());
+  this -> rb3 -> put(6);
+  this -> rb3 -> put(5);
+  EXPECT_EQ(5, this -> rb3 -> peekFront());
+  EXPECT_EQ(5, this -> rb3 -> getFront());
+  EXPECT_EQ(6, this -> rb3 -> peekFront());
+}
+
+
+TYPED_TEST(RingBufferTest, copyCtor) {
+  FAIL() << "not implemented";
+}
+
+
+TEST(DynamicRingBuffer, copyCtor){
+  DynamicRingBuffer<int> rb3(3);
+  rb3.put(9);
+  rb3.put(8);
+  rb3.put(7);
+  rb3.put(6);
+  rb3.put(5);
+  rb3.get();
+  rb3.getFront();
+
+  DynamicRingBuffer<int> rbNew(rb3);
+  EXPECT_EQ(3, rbNew.capacity());
+  EXPECT_EQ(1, rbNew.size());
+  rb3.put(4);
+  rb3.put(3);
+  rb3.get();
+  EXPECT_EQ(3, rbNew.capacity());
+  EXPECT_EQ(1, rbNew.size());
+  EXPECT_EQ(6, rbNew.get());
+  EXPECT_EQ(0, rbNew.size());
+  rbNew.put(99);
+  EXPECT_EQ(1, rbNew.size());
+  EXPECT_EQ(2, rb3.size());
+  EXPECT_EQ(4, rb3.get());
+  EXPECT_EQ(3, rb3.getFront());  
+}
+
+
+TEST(StaticRingBuffer, copyCtor){
+  StaticRingBuffer<3, int> rb3;
+  rb3.put(9);
+  rb3.put(8);
+  rb3.put(7);
+  rb3.put(6);
+  rb3.put(5);
+  rb3.get();
+  rb3.getFront();
+
+  StaticRingBuffer<3, int> rbNew(rb3);
+  EXPECT_EQ(3, rbNew.capacity());
+  EXPECT_EQ(1, rbNew.size());
+  rb3.put(4);
+  rb3.put(3);
+  rb3.get();
+  EXPECT_EQ(3, rbNew.capacity());
+  EXPECT_EQ(1, rbNew.size());
+  EXPECT_EQ(6, rbNew.get());
+  EXPECT_EQ(0, rbNew.size());
+  rbNew.put(99);
+  EXPECT_EQ(1, rbNew.size());
+  EXPECT_EQ(2, rb3.size());
+  EXPECT_EQ(4, rb3.get());
+  EXPECT_EQ(3, rb3.getFront());  
 }
